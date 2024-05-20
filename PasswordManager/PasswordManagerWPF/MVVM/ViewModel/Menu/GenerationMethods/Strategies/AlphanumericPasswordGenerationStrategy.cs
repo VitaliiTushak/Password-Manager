b4255e91ddf17b6de2ae00af87f8366e5abf6e13
Віталій Тушак <ipz224_tvs@student.ztu.edu.ua>
2024-05-20 18:54:55 +0300

    Added 3 additional strategies to generate password

---
+++ PasswordManager/PasswordManagerWPF/MVVM/ViewModel/Menu/GenerationMethods/Strategies/AlphanumericPasswordGenerationStrategy.cs
@@ -0,0 +1,18 @@
+namespace PasswordManagerWPF.MVVM.ViewModel.Menu.GenerationMethods.Strategies;
+
+public class AlphanumericPasswordGenerationStrategy : IGenerationStrategy
+{
+    private readonly Random _random = new Random();
+
+    public string GeneratePassword(int length)
+    {
+        const string alphanumericChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return GeneratePasswordFromChars(alphanumericChars, length);
+    }
+
+    private string GeneratePasswordFromChars(string chars, int length)
+    {
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[_random.Next(s.Length)]).ToArray());
+    }
+}
---
+++ PasswordManager/PasswordManagerWPF/MVVM/ViewModel/Menu/GenerationMethods/Strategies/PronounceablePasswordGenerationStrategy.cs
@@ -0,0 +1,23 @@
+namespace PasswordManagerWPF.MVVM.ViewModel.Menu.GenerationMethods.Strategies;
+
+public class PronounceablePasswordGenerationStrategy : IGenerationStrategy
+{
+    private readonly Random _random = new Random();
+    private const string Vowels = "aeiou";
+    private const string Consonants = "bcdfghjklmnpqrstvwxyz";
+
+    public string GeneratePassword(int length)
+    {
+        string password = string.Empty;
+        for (int i = 0; i < length; i++)
+        {
+            password += (i % 2 == 0) ? GetRandomChar(Vowels) : GetRandomChar(Consonants);
+        }
+        return password;
+    }
+
+    private char GetRandomChar(string charSet)
+    {
+        return charSet[_random.Next(charSet.Length)];
+    }
+}
